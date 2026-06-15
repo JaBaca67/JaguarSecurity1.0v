ef824ec7fd13e2e6c3caa78e1cde30d03f9ad1de
Eduardo <eduardo.jose@uamv.edu.ni>
2026-06-15 10:09:15 -0600

    Aca ahora si esta ya

--- JaguarSecurity1.0v/Program.cs
+++ JaguarSecurity1.0v/Program.cs
@@ -1,68 +1,60 @@
 ﻿using System;
 
-class Program
-{
-    static void Main()
-    {
-        IniciarSesionGuardia();
-    }
-
     static void IniciarSesionGuardia()
+{
+    string nombre, apellido;
+    int entrada;
+    string nomEntrada;
+    DateTime horaInicio;
+    bool sesionActiva = false;
+
+    while (!sesionActiva)
     {
-        string nombre, apellido;
-        int entrada;
-        string nomEntrada;
-        DateTime horaInicio;
-        bool sesionActiva = false;
+        Console.Write("Ingrese su nombre: ");
+        nombre = Console.ReadLine();
 
-        while (!sesionActiva)
+        Console.Write("Ingrese su apellido: ");
+        apellido = Console.ReadLine();
+
+        Console.WriteLine("\nEntrada UAM");
+        Console.WriteLine("1. Portón Principal");
+        Console.WriteLine("2. Portón Sur");
+        Console.WriteLine("3. Portón Norte");
+        Console.Write("Seleccione una entrada (1-3): ");
+        entrada = Convert.ToInt32(Console.ReadLine());
+
+        switch (entrada)
         {
-            Console.Write("Ingrese su nombre: ");
-            nombre = Console.ReadLine();
+            case 1:
+                nomEntrada = "Portón Principal";
+                break;
 
-            Console.Write("Ingrese su apellido: ");
-            apellido = Console.ReadLine();
+            case 2:
+                nomEntrada = "Portón Sur";
+                break;
 
-            Console.WriteLine("\nEntrada UAM");
-            Console.WriteLine("1. Portón Principal");
-            Console.WriteLine("2. Portón Sur");
-            Console.WriteLine("3. Portón Norte");
-            Console.Write("Seleccione una entrada (1-3): ");
-            entrada = Convert.ToInt32(Console.ReadLine());
+            case 3:
+                nomEntrada = "Portón Norte";
+                break;
 
-            switch (entrada)
-            {
-                case 1:
-                    nomEntrada = "Portón Principal";
-                    break;
+            default:
+                nomEntrada = "Desconocida";
+                break;
+        }
 
-                case 2:
-                    nomEntrada = "Portón Sur";
-                    break;
+        if (nombre != "" && apellido != "")
+        {
+            horaInicio = DateTime.Now; // Hora automática
+            sesionActiva = true;
 
-                case 3:
-                    nomEntrada = "Portón Norte";
-                    break;
-
-                default:
-                    nomEntrada = "Desconocida";
-                    break;
-            }
-
-            if (nombre != "" && apellido != "")
-            {
-                horaInicio = DateTime.Now; // Asignación automática
-                sesionActiva = true;
-
-                Console.WriteLine("\n[OK] Autenticación exitosa...");
-                Console.WriteLine("Operador: " + nombre + " " + apellido);
-                Console.WriteLine("Hora exacta: " + horaInicio);
-                Console.WriteLine("Ubicación: " + nomEntrada);
-            }
-            else
-            {
-                Console.WriteLine("\nDatos inválidos. Intente de nuevo.\n");
-            }
+            Console.WriteLine("\n[OK] Autenticación exitosa...");
+            Console.WriteLine("Operador: " + nombre + " " + apellido);
+            Console.WriteLine("Hora exacta: " + horaInicio);
+            Console.WriteLine("Ubicación: " + nomEntrada);
+        }
+        else
+        {
+            Console.WriteLine("\nDatos inválidos. Intente de nuevo.");
         }
     }
 }
