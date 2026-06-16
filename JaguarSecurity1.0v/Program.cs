544556de30f233989c923eca85fff7493fc59da1
Baca <jabaca@uamv.edu.ni>
2026-06-15 19:17:25 -0600

    Agregue mas validaciones a la funcion login, listo para pasar a la rama develop.

--- JaguarSecurity1.0v/Program.cs
+++ JaguarSecurity1.0v/Program.cs
@@ -1,7 +1,7 @@
 ﻿static void IniciarSesionGuardia()
 {
     string nombre, apellido;
-    int entrada;
+    int entrada = 0;
     string nomEntrada = "";
     DateTime horaInicio;
     bool sesionActiva = false;
@@ -9,18 +9,18 @@
     while (!sesionActiva)
     {
         Console.Write("Ingrese su nombre: ");
-        nombre = Console.ReadLine();
+        nombre = Console.ReadLine()!.Trim();// .Trim() quita espacios accidentales al inicio y final
 
         Console.Write("Ingrese su apellido: ");
-        apellido = Console.ReadLine();
+        apellido = Console.ReadLine()!.Trim();
 
         bool nombreValido = true;
         bool apellidoValido = true;
 
-        // Validar nombre
+        // Validar nombre (Ahora permite letras y espacios)
         foreach (char c in nombre)
         {
-            if (!char.IsLetter(c))
+            if (!char.IsLetter(c) && !char.IsWhiteSpace(c))
             {
                 nombreValido = false;
                 break;
@@ -30,7 +30,7 @@
         // Validar apellido
         foreach (char c in apellido)
         {
-            if (!char.IsLetter(c))
+            if (!char.IsLetter(c) && !char.IsWhiteSpace(c))
             {
                 apellidoValido = false;
                 break;
@@ -47,8 +47,15 @@
                 Console.WriteLine("2. Portón Sur");
                 Console.WriteLine("3. Portón Norte");
                 Console.Write("Seleccione una entrada (1-3): ");
-                entrada = Convert.ToInt32(Console.ReadLine());
 
+                /*Mejora de lider: Validar que la entrada sea un número entero
+                 * si el guardia ingresa un letra (ej:A), el programa no se bloquea y muestra un mensaje de error */
+
+                string entradaTexto = Console.ReadLine()!.Trim();
+                if (!int.TryParse(entradaTexto, out entrada))
+                {
+                    entrada = 0; // Forzar un valor invalido para que se repita el ciclo
+                }
                 switch (entrada)
                 {
                     case 1:
@@ -73,6 +80,7 @@
             horaInicio = DateTime.Now; // Hora automática
             sesionActiva = true;
 
+            Console.Clear(); // Limpiar pantalla para mostrar solo la información relevante después de iniciar sesión
             Console.WriteLine("\n[OK] Autenticación exitosa...");
             Console.WriteLine("Operador: " + nombre + " " + apellido);
             Console.WriteLine("Hora exacta: " + horaInicio);
